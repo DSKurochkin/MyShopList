4e002c7cbe032629c2cae400e4d6d98c25c5b1a5
Dk <kurochkin.dm@gmail.com>
2023-06-06 08:28:56 +0300

    add recycledView

--- app/src/main/java/com/example/myshoppingtasks/presentation/MaiViewModel.kt
+++ app/src/main/java/com/example/myshoppingtasks/presentation/MaiViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.myshoppingtasks.presentation
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.myshoppingtasks.data.InMemRepImpl
 import com.example.myshoppingtasks.domain.ShopItem
@@ -8,20 +7,20 @@ import com.example.myshoppingtasks.domain.uskeys.EditShopItem
 import com.example.myshoppingtasks.domain.uskeys.GetShopList
 import com.example.myshoppingtasks.domain.uskeys.RemoveShopItem
 
-class MaiViewModel:ViewModel() {
+class MaiViewModel : ViewModel() {
     private val repo = InMemRepImpl
 
     private val getShopListUseCase = GetShopList(repo)
     private val editShopItemCase = EditShopItem(repo)
     private val deleteShopItemCase = RemoveShopItem(repo)
 
-    val shopList= getShopListUseCase.getShopList()
+    val shopList = getShopListUseCase.getShopList()
 
-    fun removeItem(id: Int){
-        deleteShopItemCase.removeShopItem(id)
+    fun removeItem(item: ShopItem) {
+        deleteShopItemCase.removeShopItem(item)
     }
 
-    fun changeItemEnabled(item: ShopItem){
+    fun changeItemEnabled(item: ShopItem) {
         val copy = item.copy(isEnabled = !item.isEnabled)
         editShopItemCase.editShopItem(copy)
     }
